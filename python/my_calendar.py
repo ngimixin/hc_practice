94506fe38a2574d38f9619d83653ee9f3a9e3b4c
ngimixin <nagai.hideaki.2022@gmail.com>
2025-09-03 23:48:36 +0900

    fixup! WIP

--- python/my_calendar.py
+++ python/my_calendar.py
@@ -2,46 +2,50 @@ import sys
 from datetime import datetime, timedelta
 
 
-# month = {1: "January", 2: "February", 3: "March", 4: "April", 
-#          5: "May", 6: "June", 7: "July", 8: "August", 
-#          9: "September", 10: "October", 11: "November", 12: "December"}
+day_of_week_l = ["月", "火", "水", "木", "金", "土", "日"]
 
-day_of_week = ["月", "火", "水", "木", "金", "土", "日"]
-
-end_of_month = {28: 2,
+end_of_month_d = {28: [2],
                 30: [4, 6, 9, 11],
                 31: [1, 3, 5, 7, 8, 10, 12]}
 
+input_date = None;
+
 def main():
-    # print("test")
     # print(sys.argv) 
+    #エディタから実行の場合
     if len(sys.argv) == 1:
-        input_date = datetime.today()
-        begining_of_the_month  = input_date.replace(day=1)
-         
+        input_date = datetime.today().replace(day=1)
+        # begining_of_month = input_date.replace(day=1)
+    # コマンドライン実行の場合
     else:
         if sys.argv[1] == "-m":
-            begining_of_the_month = datetime(2025, int(sys.argv[2]), 1)
+            input_date = datetime(2025, int(sys.argv[2]), 1)
 
-    this_year = begining_of_the_month.year
-    this_month = str(begining_of_the_month.month) + "月"
-    first_weekday = begining_of_the_month.weekday()
+    begining_of_month = input_date
+    this_year = begining_of_month.year
+    this_month = begining_of_month.month
+    this_month_jp = str(begining_of_month.month) + "月"
+    first_weekday = begining_of_month.weekday()
+    first_weekday_jp = day_of_week_l[first_weekday]
+    week = " ".join(day_of_week_l)
+    
+    # 月末算出（閏年か考慮）
+    if is_leap_year(this_year):
+        end_of_month = 29
+    else:
+        key = [k for k, v in end_of_month_d.items() if this_month in v]
+        end_of_month = key[0]
+        
 
     print(f"今年：{this_year}")
-    print(f"今月：{this_month}")
-    print(f"月初曜日：{first_weekday}")
-    print(f"月初：{begining_of_the_month}")
+    print(f"今月：{this_month_jp}")
+    print(f"月初曜日：{first_weekday_jp}")
+    print(f"月初：{begining_of_month}")
+    print(f"月末：{end_of_month}")
     
 
-    print(f"    {this_month} 2025")
-    
-
-    print(begining_of_the_month)
-    # end_of_month = begining_of_the_month  + datetime.date(2015, 1, 1) - relativedelta(days=1)
-    # print(end_of_month)
-    # hantei = is_leap_year(year)
-    # print(hantei)
-    # if 
+    print(f"     {this_month_jp} 2025")
+    print(week)
 
 
 
