324665c20f79ef8e2d55b06f909c44338f837f70
ngimixin <nagai.hideaki.2022@gmail.com>
2025-09-03 20:27:22 +0900

    fixup! WIP

--- python/my_calendar.py
+++ python/my_calendar.py
@@ -1,22 +1,27 @@
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 
 month = {1: "January", 2: "February", 3: "March", 4: "April", 
          5: "May", 6: "June", 7: "July", 8: "August", 
          9: "September", 10: "October", 11: "November", 12: "December"}
 
+day_of_week = {0: "Mo", 1: "Tu", 2: "We", 3: "Th", 4: "Fr", 5: "Sa", 6: "Su"}
+
 def main():
     # print("test")
     # print(sys.argv) 
     if len(sys.argv) == 1:
-        cal_base = datetime.now()
-    # else:
-    #     if sys.argv[1] == "-m":
-    #         cal_base = datetime(2025, int(sys.argv[2]), 1)
+        input_date = datetime.now()
+        base_date = input_date.replace(day=1)
+    else:
+        if sys.argv[1] == "-m":
+            base_date = datetime(2025, int(sys.argv[2]), 1)
 
-    print(cal_base)
-    print(f"    {month[cal_base.month]} 2025")
-    print(cal_base.weekday())
+    print(base_date)
+    print(f"    {month[base_date.month]} 2025")
+    print(base_date.weekday())
+
+    print(base_date)
 
 if __name__ == "__main__":
     main()
