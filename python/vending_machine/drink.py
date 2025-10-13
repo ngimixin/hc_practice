9c7c1cf1847165234b2f730715f3068a75a49e6b
ngimixin <nagai.hideaki.2022@gmail.com>
2025-10-14 02:36:25 +0900

    feat(drink): ブランド仕様オブジェクトを実装（販売可否・販売開始日時・停止日時など）

--- python/vending_machine/drink.py
+++ python/vending_machine/drink.py
@@ -1,7 +1,19 @@
+from datetime import datetime
+from typing import Optional
+
 class Drink:
-    def __init__(self, brand, price):
+    """ブランドごとの管理情報"""
+    def __init__(
+            self,
+            brand: str,
+            price: int,
+            active: bool = True,
+            discontinued_at: Optional[datetime] = None):
         self.__brand = brand
         self.__price = price
+        self.__active = True
+        self.__created_at = datetime.now()
+        self.__discontinued_at = discontinued_at
         
     @property
     def brand(self):
@@ -11,6 +23,36 @@ class Drink:
     def price(self):
         return self.__price
     
+    
+    @property
+    def active(self):
+        return self.__active
+    
+    @property
+    def created_at(self):
+        return self.__created_at
+    
+    @property
+    def discontinued_at(self):
+        return self.__discontinued_at
+
+    @price.setter
+    def price(self, new_price):
+        self.__price = new_price 
+
+    @active.setter
+    def active(self, flag):
+        self.__active = flag
+        
+    @discontinued_at.setter
+    def discontinued_at(self, discontinued_datetime):
+        self.__discontinued_at = discontinued_datetime
+    
+    def discontinue(self) -> None:
+        """販売停止にする"""
+        self.active = False
+        self.discontinued_at = datetime.now()
+
     def __str__(self):
         return f"{self.brand}"
 
