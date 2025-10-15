fa473af888a2de8736833e3d5419d2b89d53fd00
ngimixin <nagai.hideaki.2022@gmail.com>
2025-10-16 00:57:09 +0900

    feat(drink): pk属性を追加し、在庫管理の責務をDrinkクラスに戻す

--- python/vending_machine/drink.py
+++ python/vending_machine/drink.py
@@ -1,8 +1,14 @@
 class Drink:
-    def __init__(self, brand: str, price: int):
+    def __init__(self, brand: str, price: int, quantity: int):
+        self.__pk = None
         self.__brand = brand
         self.__price = price
+        self.__quantity = quantity
         
+    @property
+    def pk(self):
+        return self.__pk
+
     @property
     def brand(self):
         return self.__brand
@@ -11,12 +17,21 @@ class Drink:
     def price(self):
         return self.__price
     
+    @property
+    def quantity(self):
+        return self.__quantity
+    
     @price.setter
-    def price(self, new_price):
-        self.__price = new_price 
-
-    def __str__(self):
-        return f"{self.brand}"
+    def price(self, price):
+        self.__price = price 
+        
+    @quantity.setter
+    def quantity(self, quantity):
+        self.__quantity = quantity
+        
+    @pk.setter
+    def pk(self, pk):
+        self.__pk = pk
 
     def __repr__(self):
-        return f"Drink(brand={self.brand!r}, price={self.price!r})"
+        return f"Drink(pk={self.pk}, brand={self.brand!r}, price={self.price}, quantity={self.quantity})"
