000e2872b2a201a12b96119ba485fee996d00f57
ngimixin <nagai.hideaki.2022@gmail.com>
2025-10-17 01:41:38 +0900

    refactor(drink): Drinkクラスを単一商品の属性のみ保持する構造に簡素化

--- python/vending_machine/drink.py
+++ python/vending_machine/drink.py
@@ -1,14 +1,8 @@
 class Drink:
-    def __init__(self, brand: str, price: int, quantity: int):
-        self.__pk = None
+    def __init__(self, brand: str, price: int):
         self.__brand = brand
         self.__price = price
-        self.__quantity = quantity
         
-    @property
-    def pk(self):
-        return self.__pk
-
     @property
     def brand(self):
         return self.__brand
@@ -17,21 +11,10 @@ class Drink:
     def price(self):
         return self.__price
     
-    @property
-    def quantity(self):
-        return self.__quantity
-    
     @price.setter
     def price(self, price):
         self.__price = price 
         
-    @quantity.setter
-    def quantity(self, quantity):
-        self.__quantity = quantity
-        
-    @pk.setter
-    def pk(self, pk):
-        self.__pk = pk
 
     def __repr__(self):
-        return f"Drink(pk={self.pk}, brand={self.brand!r}, price={self.price}, quantity={self.quantity})"
+        return f"Drink(brand={self.brand!r}, price={self.price})"
