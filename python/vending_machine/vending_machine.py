f8c7be2b03f896529f5922a76caf0d7a65e62084
ngimixin <nagai.hideaki.2022@gmail.com>
2025-10-23 01:22:31 +0900

    refactor(vending_machine): Repository の依存性注入と不要コード整理

--- python/vending_machine/vending_machine.py
+++ python/vending_machine/vending_machine.py
@@ -1,16 +1,15 @@
 from __future__ import annotations
+from drink_repository import DrinkRepository
 from drink import Drink
 from suica import Suica
-from drink_repository import DrinkRepository
 
 class SoldOutError(Exception):
     """在庫切れのときに発生する例外"""
-    pass
 
 class VendingMachine:
-    def __init__(self, initial_amount=0):
+    def __init__(self, repo: DrinkRepository, initial_amount=0):
         self.__total_amount = initial_amount
-        self.__repo = DrinkRepository()
+        self.__repo = repo
 
     @property
     def total_amount(self): 
@@ -22,7 +21,6 @@ class VendingMachine:
 
     def get_brands(self):
         """全ドリンク一覧を返す"""
-        
         inventory = self.__repo.get_all()
         brands = []
 
@@ -33,7 +31,6 @@ class VendingMachine:
 
     def get_available_brands(self):
         """購入可能なドリンク一覧を返す（現在の残高で購入可能かつ在庫が1本以上あるドリンク）"""
-        
         brands = self.get_brands()
         available_brands = []
         
@@ -44,15 +41,12 @@ class VendingMachine:
         return available_brands
     
 
-
     def restock(self, product_id: int, quantity: int) -> None:
         """商品在庫を追加するメソッド"""
-
         self.__repo.increase_stock(product_id, quantity)
 
     def vend(self, product_id: int, suica: Suica) -> Drink:
         """商品を一本購入するメソッド"""
-        
         price = self.__repo.get_price(product_id)
 
         # 残高不足時はInsufficientBalanceErrorが自動伝播
@@ -67,41 +61,7 @@ class VendingMachine:
 
         return drink
 
-
-
-# def _debug():
-#     """簡易スモークテスト"""
-#     print("=== VendingMachineクラス単体テスト ===")
-#
-#     suica = Suica(1000)
-#     ven = VendingMachine()
-#
-#     ven.add_drink("ペプシ", 150, )
-#     print("[DEBUG]", ven.inventory)
-#
-#     bought = ven.buy("ペプシ", suica)
-#     print(bought)
-#     print(repr(bought))
-#     print("在庫:", ven.get_stock("ペプシ"))
-#
-#     # 在庫が尽きるまで買ってみる
-#     while True:
-#         try:
-#             # suica.charge(20000)
-#             bought = ven.buy("ペプシ", suica)
-#             print(f"{bought}を購入。残高:{suica.balance}")
-#         except (UnknownBrandError, SoldOutError, InvalidChargeAmountError, InsufficientBalanceError) as e:
-#             print(e)
-#             break
-#
-#     # ven.get_brand_names()
-#     print("[DEBUG]", ven.inventory)
-#
-#     print("=== テスト完了 ===")
-#
 #
 # if __name__ == "__main__":
 #     _debug()
-#
-#
 #
